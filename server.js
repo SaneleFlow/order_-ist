128239ea0c419ba6fbfbaed727d186f9ddee606c
SaneleFlow <technoflow76@gmail.com>
2021-01-27 13:52:13 +0100

    here

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/list-app'));
+app.use(express.static('./dist/list-app'));
 
 
 // app.use(express.static(’./dist/<name-on-package.json>’));
