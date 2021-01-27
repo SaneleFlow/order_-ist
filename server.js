5c0c2b2aa46f4bc40fffc469ee1b638659e307b4
SaneleFlow <technoflow76@gmail.com>
2021-01-27 15:27:30 +0100

    here

--- server.js
+++ server.js
@@ -4,20 +4,32 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('/dist/list-app'));
+// Serve only the static files form the dist directory
+// app.use(express.static('./dist'));
+app.use(express.static(__dirname+'/dist/list-app'));
 
+// app.use(path.join(__dirname, "/src/index"));
+
+
+
+
+// app.use(express.static(’./dist/<name-on-package.json>’));
 
 // app.get('/*', function(req,res) {
     
-// res.sendFile(path.join(__dirname,'/src/index.html'));
+// res.sendFile(path.join(__dirname+'/dist/list-app/index.html'));
 // });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/src/index.html')); // load the single view file (angular will handle the page changes on the front-end)
+});
 
 
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: '/dist/list-app/'}
-  );
-  });
+
+// app.get('/*', function(req, res) {
+//     res.sendFile('index.html', {root: 'dist/list-app/src/'}
+//   );
+//   });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
