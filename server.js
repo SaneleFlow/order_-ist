6bb652bc5e0a42180d5d86eed87cac014bb944cf
SaneleFlow <technoflow76@gmail.com>
2021-01-27 22:05:44 +0100

    main

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-//Install express server
 const express = require('express');
-const path = require('path');
+
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/src/list-app'));
+
+app.use(express.static('./src/'));
 
 
-// app.use(express.static(’./dist/<name-on-package.json>’));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/src/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'src/' }
+  );
 });
 
-// Start the app by listening on the default Heroku port
+
 app.listen(process.env.PORT || 8080);
+
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
