5d8344f1b9392a49524299d0b176c784828f4f3a
SaneleFlow <technoflow76@gmail.com>
2021-01-27 13:44:23 +0100

    here

--- server.js
+++ server.js
@@ -10,17 +10,17 @@ app.use(express.static(__dirname + '/dist/list-app'));
 
 // app.use(express.static(’./dist/<name-on-package.json>’));
 
-// app.get('/*', function(req,res) {
+app.get('/*', function(req,res) {
     
-// res.sendFile(path.join(__dirname+'/dist/list-app/index.html'));
-// });
+res.sendFile(path.join(__dirname+'/src/index.html'));
+});
 
 
 
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/list-app/'}
-  );
-  });
+// app.get('/*', function(req, res) {
+//     res.sendFile('index.html', {root: 'dist/list-app/src/'}
+//   );
+//   });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
