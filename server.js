42c3abd7ca045e8ce38daa64f8736028c70d532f
SaneleFlow <technoflow76@gmail.com>
2021-01-28 13:24:09 +0100

    server.js path

--- server.js
+++ server.js
@@ -18,4 +18,5 @@ app.listen(process.env.PORT || 8080);
 
 
 console.log(`Running on port ${process.env.PORT || 8080}`)
-  
+
+// console.log(__dirname+`/dist/list-app`)
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,3 @@
 export const environment = {
-  production: true,
-
-  api: 'https://orderlist-angular.herokuapp.com/',
-
-redirectUrl: 'https://orderlist-angular.herokuapp.com/implicit/callback'
-
-
+  production: true
 };
