59cde4fcb8e289549371bf840a35016200c38fb2
SaneleFlow <technoflow76@gmail.com>
2021-01-30 02:34:05 +0100

    heroku dependencies

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.get('/*', function (req, res) {
 app.listen(process.env.PORT || 8080);
 
 
-console.log(`Running on port ${process.env.PORT || 8080}`);
+// console.log(`Running on port ${process.env.PORT || 8080}`);
 
 
