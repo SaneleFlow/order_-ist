60097d3228529347c65b48709cb369a9411a6813
SaneleFlow <technoflow76@gmail.com>
2021-02-02 08:12:19 +0100

    cors

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+
 
 
 const app = express();
+
+
+ 
+app.use(cors())
 
 
 app.use(express.static(__dirname+'/dist/list-app'));
